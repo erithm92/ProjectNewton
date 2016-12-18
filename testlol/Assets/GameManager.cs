516934a4fd17fcd9aa1ae8150c49f525605b261e
Erith Margolis <dischordiandesigns@gmail.com>
2016-12-18 11:25:10 -0800

    added goal
    ...and failed api test

--- testlol/Assets/GameManager.cs
+++ testlol/Assets/GameManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using LoLSDK;
 
 public class GameManager : MonoBehaviour {
-    Text forceText, chargeText, pauseText;
-    public GameObject forceUI, chargeUI, pauseUI;
+    Text forceText, chargeText, pauseText, winText;
+    public GameObject forceUI, chargeUI, pauseUI, winUI;
     public GameObject paddle;
 	// Use this for initialization
 	void Start ()
     {
-	    if (forceUI == null)
+        LOLSDK.Init("com.margolisdesign.projectnewton");
+        if (forceUI == null)
            forceUI = GameObject.Find("forceText");
         forceText = forceUI.GetComponent<Text>();
         if (chargeUI == null)
@@ -18,6 +21,10 @@ public class GameManager : MonoBehaviour {
         if (pauseUI == null)
             pauseUI = GameObject.Find("pauseText");
         pauseText = pauseUI.GetComponent<Text>();
+        if (winUI == null)
+            winUI = GameObject.Find("winText");
+        winText = winUI.GetComponent<Text>();
+        winUI.SetActive(false);
     }
 	
 	// Update is called once per frame
@@ -26,5 +33,14 @@ public class GameManager : MonoBehaviour {
         forceText.text = "Force\n" + paddle.GetComponent<PaddleBehavior>().force + "";
         chargeText.text = "Charges\n" + paddle.GetComponent<PaddleBehavior>().charges + "";
         pauseText.text = "Pauses\n" + paddle.GetComponent<PaddleBehavior>().pauses + "";
+    }
+    public void Goal()
+    {
+        winUI.SetActive(true);
+        LOLSDK.Instance.CompleteGame();
+    }
+    public void Reload()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- testlol/Assets/Goal.cs
+++ testlol/Assets/Goal.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Goal : MonoBehaviour {
-
+public class Goal : MonoBehaviour
+{
+    GameObject gm;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        gm = GameObject.Find("GameManager");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Ball")
+            gm.GetComponent<GameManager>().Goal();
+    }
 }
